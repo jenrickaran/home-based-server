27f7ddf99290597a72827cf09a83a9cb9cf2dec0
Exodus102 <145414466+Exodus102@users.noreply.github.com>
2026-09-13 22:32:14 +0800

    Add Tailwind CSS integration and create initial HTML template
    - Added package.json to manage Tailwind CSS dependencies.
    - Created input.css to import Tailwind CSS styles.
    - Generated output.css with Tailwind CSS styles.
    - Added index.php as the main HTML template, linking to the generated CSS.

---
+++ app.py
@@ -0,0 +1,38 @@
+import socket
+from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+
+hostName = socket.gethostname()
+ip = socket.gethostbyname(hostName)
+load_dotenv()
+checkUsername = os.getenv("USER")
+checkPassword = os.getenv("PASSWORD")
+
+print("\n")
+print("===============================")
+print("Name and IP Adress of the Server")
+print(hostName)
+print(ip)
+print("===============================")
+print("\n")
+print("===============================")
+print("Log in to open the server")
+username = input("Username: ")
+password = input("Password: ")
+print("===============================")
+print(checkUsername)
+print(checkPassword)
+
+if username == checkUsername and password == checkPassword:
+    app = Flask(__name__)
+
+    @app.route("/")
+    def home():
+        return render_template("index.php")
+
+    if __name__ == "__main__":
+        app.run(host=ip, port=8001)
+else:
+    print("You entered wrong credentials.")
---
+++ templates/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{url_for('static', filename='src/output.css') }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME DRIVE</title>
+</head>
+
+<body>
+    <span>TEsting</span>
+</body>
+
+</html>
